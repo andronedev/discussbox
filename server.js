8ff8459fed6ba702c3330e2534dbfdb999c3c349
Glitch (hello-express) <none>
2021-01-28 20:14:08 +0000

    🎰🔖 Checkpoint ./views/index.html:25400799/190 ./server.js:25400799/5401

--- server.js
+++ server.js
@@ -1,41 +1,42 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 // ajout de socket.io
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+var connectCounter = 0;
+var messages = [];
 
-var messages = []
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.get("/", function(req, res) {
+  res.sendFile("/views/index.html", { root: __dirname });
+});
 
+app.get("/json", function(req, res) {
+  res.status(200).json({ message: "ok" });
+});
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(express.static('public'))
-app.get('/', function (req, res) {
-   res.sendFile('/views/index.html', { root: __dirname })
-})
+setInterval(function() {
+  io.emit("NBONLINE", io.engine.clientsCount);
+}, 1000);
 
-app.get('/json', function (req, res) {
-   res.status(200).json({"message":"ok"})
-})
-
-io.on('connection', (socket) =>{
-   console.log(`Connecté au client ${socket.id}`)
-   // émission d'un évènement
+io.on("connection", socket => {
+  // émission d'un évènement
+  io.emit("NBONLINE", io.engine.clientsCount);
   
-   io.emit('NEWS',"Une personne viens de se connecter")
-  
-  
-  socket.on('SEND_MESSAGE', function(data) {
-    messages.push(data)
-        io.emit('MESSAGE', data)
-    });
-   socket.on('GET_MESSAGES', function(data) {
-        io.emit('MSGS', {id:data,messages:messages})
-    });
-})
+
+  socket.on("SEND_MESSAGE", function(data) {
+    messages.push(data);
+    io.emit("MESSAGE", data);
+  });
+  socket.on("GET_MESSAGES", function(data) {
+    io.emit("MSGS", { id: data, messages: messages });
+  });
+});
 
 // on change app par server
-server.listen(3000, function () {
- console.log('Votre app est disponible sur localhost:3000 !')
-})
+server.listen(3000, function() {
+  console.log("Votre app est disponible sur localhost:3000 !");
+});
